3521453639e1a25dfd22bd1105238f910362e4f7
khyathiraghavi <khythiraghavi@gmail.com>
2018-04-05 13:38:03 -0400

    done khyathi

--- bilstm_charcnn/modelach.py
+++ bilstm_charcnn/modelach.py
@@ -7,6 +7,7 @@ from collections import Counter, defaultdict
 import random
 import sys
 import argparse
+import numpy
 import numpy as np
 import torch
 from torch import nn
@@ -33,7 +34,7 @@ TIMEOUT = 300000
 
 # format of files: each line is "word1|tag2 word2|tag2 ..."
 train_file = "../data/kaggle/train.csv"
-dev_file = "/Users/abhishek/Desktop/Toxic-Comment-Classification/offensive-language/data/kaggle/dev.csv"
+dev_file = "../data/kaggle/dev.csv"
 #train_file = "train.txt"
 #dev_file = "dev.txt"
 
@@ -94,8 +95,11 @@ def read(fname):
 
 train = list(myread1(train_file))
 print(len(train))
-dev = list(train[:int(len(train)/5)])
-train = list(train[int(len(train)/5):])
+#dev = list(train[:int(len(train)/5)])
+#train = list(train[int(len(train)/5):])
+train = train[0:20]
+dev = train[0:20]
+
 words = []
 tags = []
 chars = set()
@@ -205,39 +209,48 @@ print("startup time: %r" % (time.time() - start))
 start = time.time()
 i = all_time = dev_time = all_tagged = this_tagged = this_loss = 0
 
-for ITER in range(3):
+batch_size = 1
+log_after = 1
+for ITER in range(100):
     count = 0
     epoch_loss = 0
     correct = 0
     total_correct = 0
     #random.shuffle(train)
+    batch_count = 0
     
     for s in train:
+	batch_count +=1
         i += 1
         #print(i)
+	'''
         if i % 500 == 0:
             all_tagged += this_tagged
             this_loss = this_tagged = 0
             all_time = time.time() - start
-        if i % 10000 == 0 or all_time > TIMEOUT:  # eval on dev
-            fout = open("output1.txt".format(i), "w")
-            dev_start = time.time()
-            good_sent = bad_sent = good = bad = 0.0
+	'''
+        if i % log_after == 0 :  # eval on dev
+            fout = open("output"+str(i)+".txt", "w")
+            #dev_start = time.time()
+            #good_sent = bad_sent = good = bad = 0.0
             for sent in dev:
-                words, golds = zip(*sent)
-                tags = [vt.i2w[i] for i in model(words, volatile=True).max(1)[1].cpu().data.view(-1)]
-                fout.write(str(tags) +"\n")
-                if tags == list(golds): good_sent += 1
-                else: bad_sent += 1
-                for go, gu in zip(golds, tags):
-                    if go == gu: good += 1
-                    else: bad += 1
+                #words, golds = zip(*sent)
+		words = sent[0]
+		tags = sent[1]
+		preds = model(words, volatile=True)
+            	devgolds = get_var(torch.FloatTensor([1]))
+        	if tags == '1' or tags==1:
+            		devgolds = get_var(torch.FloatTensor([1]))
+        	else:
+            		devgolds = get_var(torch.FloatTensor([0]))
+		p = preds.data.numpy()[0]
+		if float(p) < 0.5:
+		    fout.write("0\n")
+		else:
+		    fout.write("1\n")
+	    #dev_loss += loss(preds, devgolds)
+            #print ("Dev Loss: ",dev_loss.data.view(-1))
             fout.close()
-            dev_time += time.time() - dev_start
-            train_time = time.time() - start - dev_time
-            print ("tag_acc=%.8f, sent_acc=%.4f, time=%.4f, word_per_sec=%.4f" % (good/(good+bad), good_sent/(good_sent+bad_sent), train_time, all_tagged/train_time))
-            if all_time > TIMEOUT:
-                sys.exit(0)
 
 
         # batch / loss
@@ -246,7 +259,7 @@ for ITER in range(3):
         tags = s[1]
         count = count + len(words)
         preds = model(words)
-        print (preds)
+        #print (preds)
         #golds = [tags]
 
         #golds = get_var(torch.LongTensor([0,0]))
@@ -264,14 +277,14 @@ for ITER in range(3):
         #predicted = pred.eq(Y.data.view_as(pred))
         #correct += predicted.sum()
         print (preds.size())
-        print (Y.size())
-        print("Y is ",Y)
-        print("preds is",preds)
+        #print (Y.size())
+        #print("Y is ",Y)
+        #print("preds is",preds)
         #exit(1)
         #loss = F.cross_entropy(preds, Y)
         
         loss1 = loss(preds, Y)
-        print ("Loss is ",loss1)
+        print ("Train Loss: ",loss1.data.view(-1))
         #exit(1)
         nn.utils.clip_grad_norm(model.parameters(), 0.25)
         # log / optim
@@ -284,4 +297,3 @@ for ITER in range(3):
     torch.save(model.state_dict(), './current_model1.pt')
     print("epoch %r finished" % ITER)
     print("Epoch loss : ",  epoch_loss /count)
-
